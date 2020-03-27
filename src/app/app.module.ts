465473158e7c4ede63dc78201d7656b3671be973
Vuthuyhangbamboo <hangvtph07414@fpt.edu.vn>
2020-03-27 15:50:13 +0700

    3 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { HelloComponent } from './hello.component';
 import { NavComponent } from './nav/nav.component';
 import { BannerComponent } from './banner/banner.component';
 import { ProductComponent } from './product/product.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NavComponent, BannerComponent, ProductComponent ],
+  declarations: [ AppComponent, HelloComponent, NavComponent, BannerComponent, ProductComponent, FooterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
